7024ff87f1705fcba5dfdcf70ca82ef2e1cb82fd
Zhiqing.F <95883827+zhiqingfeng@users.noreply.github.com>
2023-09-12 16:49:58 +0300

    Updated

---
+++ W3_Model&View/HelloThymeleaf/src/main/java/com/example/HelloThymeleaf/HelloThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.HelloThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloThymeleafApplication.class, args);
+	}
+
+}
