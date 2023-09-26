4db0b1854253a4cf97f43750ee05179df9177c9c
Zhiqing.F <95883827+zhiqingfeng@users.noreply.github.com>
2023-09-26 19:41:23 +0300

    fixed

---
+++ W5_RestService/Bookstore_fixed/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -0,0 +1,36 @@
+package com.example.Bookstore;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	@Bean //i forgot to add this, that's why the booklist do not display
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+		return(args) -> {
+		
+			crepository.save(new Category("Humous"));
+			crepository.save(new Category("Animal"));
+			
+			repository.save(new Book("Ernest Hemingway", "A Farewell to Armns", "1232323-21", 1929, crepository.findByName("Animal").get(0)));
+			repository.save(new Book("George Orwell", "Animal Farm", "2212343-5", 1945, crepository.findByName("Humous").get(0)));
+			
+			System.out.println("IN COMMAND LINE RUNNER");
+			
+			for(Book book : repository.findAll()) {
+				System.out.println(book.toString());
+			}
+		};
+	}
+}
---
+++ W5_RestService/Bookstore_fixed/src/main/java/com/example/Bookstore/web/BookController.java
@@ -0,0 +1,83 @@
+package com.example.Bookstore.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.CategoryRepository;
+
+@Controller
+public class BookController {
+
+	@Autowired
+	private BookRepository repository;
+
+	@Autowired
+	private CategoryRepository crepository;
+	
+	//List all the books
+	@RequestMapping(value= "/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
+	}
+	
+	//RESTful service to get all books 
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+		return (List<Book>) repository.findAll();
+	}
+	
+	//RESTful service to get book by id
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId){
+		return repository.findById(bookId);
+	}
+	
+	//Add a new book
+	@RequestMapping(value="/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		model.addAttribute("categorys", crepository.findAll());
+		return "addbook";
+	}
+	
+	//Save the new book
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	//Delete the book
+	@RequestMapping(value="/delete/{id}",method=RequestMethod.GET)
+	public String deleteBook(@PathVariable("id")Long bookId, Model model ) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	//Edit the book
+	@GetMapping("/editbook/{id}")
+	public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+	    model.addAttribute("book", repository.findById(id).get());
+	    model.addAttribute("categorys", crepository.findAll());
+	    return "editbook";
+	  }
+	  
+	@PostMapping("/update/{id}")
+	public String updateUser( Book book) {	          	   
+		repository.save(book);
+	    return "redirect:/booklist";
+	}
+}
